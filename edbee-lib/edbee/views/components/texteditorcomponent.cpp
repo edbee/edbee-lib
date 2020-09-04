a95706d621e4610c5fe66a339bf9f9cea4c0a32d
Rick Blommers <rick@blommersit.nl>
2020-09-04 10:34:09 +0200

    Disabled TextEditorComponent::inputMethodEvent for Linux

--- edbee-lib/edbee/views/components/texteditorcomponent.cpp
+++ edbee-lib/edbee/views/components/texteditorcomponent.cpp
@@ -339,9 +339,13 @@ void TextEditorComponent::keyReleaseEvent(QKeyEvent *event)
 
 void TextEditorComponent::inputMethodEvent( QInputMethodEvent* m )
 {
+ #ifndef Q_OS_LINUX
 
-    // replace the selection with an empty text
-    if( textSelection()->hasSelection() ) {
+/// TODO: https://doc.qt.io/qt-5/qinputmethodevent.html
+/// Analyize how to implement this. The preeditString should NOT alter the undo-buffer
+
+    // replace the selection with an empty text (only if there's content to replace)
+    if( textSelection()->hasSelection() && (!m->preeditString().isEmpty() || !m->commitString().isEmpty())) {
         controller()->replaceSelection("",false);
     }
 
@@ -361,6 +365,9 @@ void TextEditorComponent::inputMethodEvent( QInputMethodEvent* m )
         controller()->replaceSelection(m->commitString(),false);
     }
 
+    m->accept();
+
+ #endif
 }
 
 
