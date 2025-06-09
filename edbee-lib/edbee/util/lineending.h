faf593f7ce4a41894b657c2ac1f679b336010c30
Rick Blommers <rick@blommersit.nl>
2025-06-09 20:50:18 +0200

    lineending

--- edbee-lib/edbee/util/lineending.h
+++ edbee-lib/edbee/util/lineending.h
@@ -19,40 +19,30 @@ public:
         TypeCount
     };
 
-
 protected:
 
-    explicit LineEnding( LineEnding::Type ending, const char* chars, const char* escaped_chars, const char* name );
+    explicit LineEnding(LineEnding::Type ending, const char* chars, const char* escaped_chars, const char* name);
     virtual ~LineEnding();
 
 public:
     static LineEnding* types();
     static int typeCount() { return TypeCount; }
-    static LineEnding* get( int idx );
+    static LineEnding* get(size_t idx);
 
     static LineEnding* unixType();
     static LineEnding* windowsType();
     static LineEnding* macClassicType();
 
+    static LineEnding* detect(const QString& str, LineEnding* unkownEnding = nullptr);
 
-    static LineEnding* detect( const QString& str, LineEnding* unkownEnding=0 );
-
-
-    /// This method returns the type
     virtual LineEnding::Type type() const { return type_; }
 
-    /// This method returns the chars
     virtual const char* chars() const { return charsRef_; }
 
-    /// This method returns the escaped chars
     virtual const char* escapedChars() const { return escapedCharsRef_; }
 
     /// This method returns the name of the line ending
     virtual const char* name() const { return nameRef_; }
-
-
- //   virtual LineEnding detectLineEnding
-//    virtual static QString lineEndingChars( LineEnding::Type endingType  );
 
 private:
 
