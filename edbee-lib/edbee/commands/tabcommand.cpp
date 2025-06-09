5d99c74ec5328d014afd0db8e9461a898bd4658e
Rick Blommers <rick@blommersit.nl>
2025-06-09 10:46:14 +0200

    tab command type fixes, and strange bug that tab isn't always applied

--- edbee-lib/edbee/commands/tabcommand.cpp
+++ edbee-lib/edbee/commands/tabcommand.cpp
@@ -18,15 +18,15 @@ namespace edbee {
 /// @param direction the direction of the tab (Forward or Backward)
 /// @param insertTab should we use a tab character?
 TabCommand::TabCommand(TabCommand::Direction direction, bool insertTab)
-    : dir_( direction )
-    , insertTab_( insertTab )
+    : dir_(direction)
+    , insertTab_(insertTab)
 {
 }
 
 
 /// indents or outdents in the given controller
 /// @parma controller the controller
-void TabCommand::indent( TextEditorController* controller )
+void TabCommand::indent(TextEditorController* controller)
 {
     TextDocument* doc  = controller->textDocument();
     TextSelection* sel = controller->textSelection();
@@ -34,69 +34,68 @@ void TabCommand::indent( TextEditorController* controller )
     bool useTab = false; //doc->config()->useTabChar();
     QString tab = useTab ? "\t" : QStringLiteral(" ").repeated(tabSize);
 
-    controller->beginUndoGroup( new MergableChangeGroup( controller ) );
+    controller->beginUndoGroup(new MergableChangeGroup(controller));
 
     sel->beginChanges();
 
-    int lastLine = -1;
-    for( int i=0, cnt = sel->rangeCount(); i<cnt; ++i ) {
+    ptrdiff_t lastLine = -1;
+    for (size_t i = 0, cnt = sel->rangeCount(); i < cnt; ++i) {
         TextRange& range = sel->range(i);
 
         // calculate the start and endline
-        int startLine = doc->lineFromOffset( range.min() );
-        int endLine   = doc->lineFromOffset( range.max() );
+        size_t startLine = doc->lineFromOffset(range.min());
+        size_t endLine   = doc->lineFromOffset(range.max());
 
         // when the last line is blank, we do NOT want to indent it (fixes #61)
-        if( startLine!=endLine && doc->columnFromOffsetAndLine(range.max(),endLine) == 0 ) {
+        if (startLine != endLine && doc->columnFromOffsetAndLine(range.max(), endLine) == 0) {
             --endLine;
         }
 
         // make sure we at least indent one line
-        if( startLine <= lastLine ) startLine = lastLine + 1;
+        if (static_cast<ptrdiff_t>(startLine) <= lastLine) startLine = static_cast<size_t>(lastLine + 1);
 
         // now indent all lines
-        for( int line=startLine; line<=endLine; ++line ) {
-            int offset = doc->offsetFromLine(line);
+        for (size_t line = startLine; line <= endLine; ++line) {
+            size_t offset = doc->offsetFromLine(line);
 
             // insert-tab
-            if( dir_ == Forward ) {
-                doc->replace(offset,0, tab );
-                sel->changeSpatial( offset, 0, tab.length(), true );
+            if (dir_ == Forward) {
+                doc->replace(offset, 0, tab);
+                sel->changeSpatial(offset, 0, static_cast<size_t>(tab.length()), true);
 
             // remove tab
             } else {
-                int offsetNextLine = doc->offsetFromLine(line+1)-1;
-                int endOffset = offset;
-                for( int j=0; j<tabSize; ++j ) {
-                    int off = offset + j;
-                    if(off >= doc->length()) break;
-                    QChar chr = doc->charAt(off);
+                size_t offsetNextLine = doc->offsetFromLine(line + 1) - 1;
+                size_t endOffset = offset;
+                for (size_t j = 0, cnt = static_cast<size_t>(tabSize); j < cnt; ++j ) {
+                    size_t off = offset + j;
+                    if (off >= doc->length()) break;
 
-                    if( chr != ' ' && chr != '\t' ) break;
-                    if( off >= offsetNextLine ) break;
+                    QChar chr = doc->charAt(off);
+                    if (chr != ' ' && chr != '\t' ) break;
+                    if (off >= offsetNextLine) break;
                     ++endOffset;
-                    if( chr == '\t' ) break;
+                    if (chr == '\t') break;
                 }
-                if( offset != (endOffset-offset)) {
-                    doc->replace( offset, endOffset-offset, "" );
-                    sel->changeSpatial( offset, endOffset-offset, 0, true );
+
+                if (offset < endOffset) {
+                    doc->replace(offset, endOffset - offset, "");
+                    sel->changeSpatial(offset, endOffset - offset, 0, true);
                 }
             }
-
         }
-        lastLine = endLine;
+        lastLine = static_cast<ptrdiff_t>(endLine);
     }
-
     sel->endChanges();
 
-    controller->endUndoGroup( CoalesceId_Indent + dir_, true );
+    controller->endUndoGroup(CoalesceId_Indent + dir_, true);
 }
 
 
 /// TODO: Refactor :)
 void TabCommand::execute(TextEditorController* controller)
 {
-    if( !insertTab_ ) {
+    if (!insertTab_) {
         indent(controller);
         return;
     }
@@ -111,21 +110,21 @@ void TabCommand::execute(TextEditorController* controller)
     // check if the changes are multi-line
     bool multiLine = false;
     bool atStartOfLine = true;
-    for( int i=0, cnt = sel->rangeCount(); i<cnt && !multiLine; ++i ) {
+    for (size_t i=0, cnt = sel->rangeCount(); i < cnt && !multiLine; ++i) {
         TextRange& range = sel->range(i);
         multiLine = multiLine || doc->lineFromOffset(range.anchor()) != doc->lineFromOffset(range.caret());
 
         // no selection? we need to check if we are at the start of the line
-        if( !range.hasSelection() && atStartOfLine ) {
-            int pos = range.caret();
-            int startPos = doc->offsetFromLine( doc->lineFromOffset( pos ) );
+        if (!range.hasSelection() && atStartOfLine) {
+            size_t pos = range.caret();
+            size_t startPos = doc->offsetFromLine( doc->lineFromOffset(pos));
             --pos;  // look hat the character before the pos
 
-            //Q_ASSERT(startPos <= pos);
-            if( pos >= startPos ) {
-                while(pos>startPos && atStartOfLine ) {
+            // Q_ASSERT(startPos <= pos);
+            if (pos >= startPos) {
+                while (pos>startPos && atStartOfLine) {
                     QChar chr = doc->charAt(pos);
-                    if( !ws.contains(chr) ) {
+                    if (!ws.contains(chr)) {
                         atStartOfLine = false;
                     }
                     --pos;
@@ -138,32 +137,31 @@ void TabCommand::execute(TextEditorController* controller)
     }
 
     // a Line-moving operation
-    if( multiLine || ( atStartOfLine && dir_ == Backward ) ) {
-        indent( controller );
+    if (multiLine || (atStartOfLine && dir_ == Backward)) {
+        indent(controller);
 
     // a character inserting operation
     } else {
-        if( dir_ == Forward) {
-            if( useTab ) {
-                controller->replaceSelection("\t", CoalesceId_Indent + SubCoalesceId_Indent_InsertTab );
+        if (dir_ == Forward) {
+            if (useTab) {
+                controller->replaceSelection("\t", CoalesceId_Indent + SubCoalesceId_Indent_InsertTab);
             } else {
 
                 // we need to 'calculate' the number of spaces to add (per caret)
                 QStringList texts;
-                for( int i=0, cnt = sel->rangeCount(); i<cnt; ++i ) {
+                for (size_t i = 0, cnt = sel->rangeCount(); i < cnt; ++i) {
                     TextRange& range = sel->range(i);
-                    int offset = range.min();
-                    int col = doc->columnFromOffsetAndLine(offset);
+                    size_t offset = range.min();
+                    size_t col = doc->columnFromOffsetAndLine(offset);
 
                     // calculate the number of spaces depending on the column
-                    int spaces = tabSize - col%tabSize;
-                    if( !spaces ) spaces = tabSize;
+                    int spaces = tabSize - static_cast<ptrdiff_t>(col) % tabSize;
+                    if (!spaces) spaces = tabSize;
 
                     // append the text
-                    texts.push_back( QStringLiteral(" ").repeated(spaces) );
+                    texts.push_back(QStringLiteral(" ").repeated(spaces));
                 }
-
-                controller->replaceSelection( texts, CoalesceId_Indent + SubCoalesceId_Indent_InsertSpaces  );
+                controller->replaceSelection(texts, CoalesceId_Indent + SubCoalesceId_Indent_InsertSpaces );
             }
         }
     }
