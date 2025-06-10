14b4af3ccb2e5b74f7de3431a1ab2efe9e9c110f
Rick Blommers <rick@blommersit.nl>
2025-06-10 21:16:26 +0200

    textundoestacktest textlinedatatest

--- edbee-test/edbee/models/textlinedatatest.cpp
+++ edbee-test/edbee/models/textlinedatatest.cpp
@@ -13,11 +13,11 @@
 
 namespace edbee {
 
-static QString filledTestString( const TextLineDataManager& ldm )
+static QString filledTestString( const TextLineDataManager& ldm)
 {
     QString str;
-    for( int i=0; i<ldm.length(); ++i ) {
-        if( ldm.at(i) ) {
+    for (size_t i = 0; i < ldm.length(); ++i) {
+        if (ldm.at(i)) {
             str.append("X");
         } else {
             str.append("-");
@@ -26,71 +26,82 @@ static QString filledTestString( const TextLineDataManager& ldm )
     return str;
 }
 
-/// A special class for detecting 'destructor' and destroy calls
+
+/// Detecting 'destructor' and destroy calls
 class DestructorTestList : public TextLineDataList
 {
 public:
-    DestructorTestList( int& destroyRef, int& destructorRef) : destroyCallCountRef_(destroyRef), destructorCallCountRef_(destructorRef) {}
-    virtual ~DestructorTestList() { ++destructorCallCountRef_; }
-    virtual void destroy( TextLineDataManager* manager ) { Q_UNUSED(manager); ++destroyCallCountRef_; }
+    DestructorTestList(int& destroyRef, int& destructorRef)
+        : destroyCallCountRef_(destroyRef)
+        , destructorCallCountRef_(destructorRef) {}
+
+    virtual ~DestructorTestList()
+    {
+        ++destructorCallCountRef_;
+    }
+
+    virtual void destroy(TextLineDataManager* manager)
+    {
+        Q_UNUSED(manager);
+        ++destroyCallCountRef_;
+    }
+
 private:
     int& destroyCallCountRef_;
     int& destructorCallCountRef_;
 };
 
 
-
 /// Tests the line data manager
 void TextLineDataTest::testLineDataManager()
 {
     TextLineDataManager ldm;
-    testEqual( filledTestString(ldm ), "-");
+    testEqual(filledTestString(ldm ), "-");
 
     // insert a line
-    ldm.fillWithEmpty(0,0,2);
-    testEqual( filledTestString(ldm ), "---");
+    ldm.fillWithEmpty(0, 0, 2);
+    testEqual(filledTestString(ldm ), "---");
 
     // set an item
-    int destroyCount=0;
-    int destructCount=0;
+    int destroyCount = 0;
+    int destructCount = 0;
     DestructorTestList* list = new DestructorTestList(destroyCount, destructCount);
-    ldm.giveList(1, list );
-    testEqual( filledTestString(ldm ), "-X-");
-    testEqual( destroyCount, 0 );
-    testEqual( destructCount, 0 );
-
+    ldm.giveList(1, list);
+    testEqual(filledTestString(ldm), "-X-");
+    testEqual(destroyCount, 0);
+    testEqual(destructCount, 0);
 
     // inserting an item should move the data items
-    ldm.fillWithEmpty(1,0,1);
-    testEqual( filledTestString(ldm ), "--X-");
-    testEqual( destroyCount, 0 );
-    testEqual( destructCount, 0 );
+    ldm.fillWithEmpty(1, 0, 1);
+    testEqual(filledTestString(ldm), "--X-");
+    testEqual(destroyCount, 0);
+    testEqual(destructCount, 0);
 
     // taking a list item sould not call the destructor or destroy method
-    testEqual( (quintptr)list, (quintptr)ldm.takeList(2) );
-    testEqual( filledTestString(ldm ), "----");
-    testEqual( destroyCount, 0 );
-    testEqual( destructCount, 0 );
+    testEqual((quintptr)list, (quintptr)ldm.takeList(2));
+    testEqual(filledTestString(ldm ), "----");
+    testEqual(destroyCount, 0);
+    testEqual(destructCount, 0);
 
     // giving a list back should work
-    ldm.giveList(1, list );
-    testEqual( filledTestString(ldm ), "-X--");
-    testEqual( destroyCount, 0 );
-    testEqual( destructCount, 0 );
+    ldm.giveList(1, list);
+    testEqual( filledTestString(ldm), "-X--");
+    testEqual(destroyCount, 0);
+    testEqual(destructCount, 0);
 
     // fill with empty call destroy and the destructor of the list
-    ldm.fillWithEmpty(1,2,0);
-    testEqual( filledTestString(ldm ), "--");
-    testEqual( destroyCount, 1 );
-    testEqual( destructCount, 1 );
+    ldm.fillWithEmpty(1, 2, 0);
+    testEqual(filledTestString(ldm), "--");
+    testEqual(destroyCount, 1 );
+    testEqual( destructCount, 1);
     qDebug() << "E";
 }
 
 /// References issue #66 github
 /// Calling setText() with a shorter text (less lines) then the original
-//Undo it
-//Cut a text (I suspect multiple lines)
-//And call setText() again with a shorter text
+/// - Undo it
+/// - Cut a text (I suspect multiple lines)
+/// - And call setText() again with a shorter text
 void TextLineDataTest::testSetTextLineDataIssue66()
 {
     TextEditorWidget widget;
@@ -100,20 +111,14 @@ void TextLineDataTest::testSetTextLineDataIssue66()
 
     doc->setText("a\nb\nc");
     testEqual(filledTestString(*tdm), "---");
-    qDebug() << "- a:" <<  filledTestString(*tdm);
 
     doc->setText("a\nb");
     testEqual(filledTestString(*tdm), "--");
-    qDebug() << "- b:" <<  filledTestString(*tdm);
-    qDebug() << "G";
 
     controller->undo();
-    testEqual( filledTestString(*tdm), "---");
-    qDebug() << "- c:" <<  filledTestString(*tdm);
-    qDebug() << "H";
+    testEqual(filledTestString(*tdm), "---");
 
     doc->setText("a");
-    qDebug() << "I";
 }
 
 } // edbee
