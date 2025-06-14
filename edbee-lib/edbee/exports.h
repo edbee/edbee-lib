3123b2d62b58e50ecbb5c342be4c78004112b82a
Rick Blommers <rick@blommersit.nl>
2025-06-14 20:55:53 +0200

    add (clang) compiler validation to edbee pro file

--- edbee-lib/edbee/exports.h
+++ edbee-lib/edbee/exports.h
@@ -3,6 +3,26 @@
 
 #pragma once
 
+// code for stricter compilation and testing of validation
+// A bit of a hack to place this in export.h.
+// This file is included by every header file in edbee.
+// Here it is possible to configure the correct validation to perform fixes that are required by every file
+#ifdef __clang__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wsign-conversion"
+#pragma GCC diagnostic ignored "-Wshorten-64-to-32"
+// include headers to prevent cast-errors in Qt Header
+#include <QDebug>
+#include <QtCore/qglobal.h>
+#include <QHash>
+#include <QList>
+#include <QString>
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+#include <qtmochelpers.h>
+#endif
+#pragma GCC diagnostic pop
+#endif
+
 #include <QtCore/qglobal.h>
 
 #ifdef EDBEE_SHARED
